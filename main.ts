6f75365c9e6a0d79e22ffdd81a11bdba8e391e29
tn9901236 <72171569+tn9901236@users.noreply.github.com>
2021-05-29 10:17:12 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("microbit")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
